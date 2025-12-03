31bac03b72974ec7b329dd3533256adc2b007e18
Daniel Stevens <65318437+dboone323@users.noreply.github.com>
2025-12-03 08:26:27 -0600

    chore: update all

--- Tools/ProjectScripts/Automation/project_config.sh
+++ Tools/ProjectScripts/Automation/project_config.sh
@@ -5,4 +5,4 @@ export ENABLE_AUTO_BUILD=true
 export ENABLE_AI_ENHANCEMENT=true
 export ENABLE_AUTO_TEST=true
 export PROJECT_NAME="HabitQuest"
-export PROJECT_DIR="/Users/danielstevens/Desktop/Quantum-workspace/Projects/HabitQuest"
+export PROJECT_DIR="/Users/danielstevens/Desktop/github-projects/tools-automation/Projects/HabitQuest"
